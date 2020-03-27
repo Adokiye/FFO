809e49400fbe0903477056add61f0643d04049de
Adokiye <gorgeceo@gmail.com>
2020-03-27 13:58:37 +0100

    Addition of addedFoodBox.dart

---
+++ lib/components/FoodBox/AddedFoodBox/addedFoodBox.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'styles.dart';
+
+class AddedFoodBox extends StatelessWidget {
+  final String text;
+  final String imageUrl;
+  final GestureTapCallback onPressed;
+  AddedFoodBox({@required this.text, @required this.onPressed, this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+        width: MediaQuery.of(context).size.width * 0.85,
+        height: 69.0,
+        decoration: boxDecorationStyle,
+        child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 21.0),
+            child: Row(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Container(
+                    margin: EdgeInsets.only(right: 10),
+                    child: Image.asset(
+                      imageUrl,
+                      height: 30,
+                      width: 30,
+                      fit: BoxFit.contain,
+                    )),
+                Text(
+                  text,
+                  style: foodNameStyle,
+                ),
+                Spacer(),
+                Material(
+                    child: InkWell(
+                        onTap: onPressed,
+                        child: Icon(
+                          Icons.delete_outline,
+                          color: const Color(0xffEF383F),
+                          size: 20.0,
+                        ))),
+              ],
+            )));
+  }
+}
---
+++ lib/components/FoodBox/AddedFoodBox/styles.dart
@@ -0,0 +1,11 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+
+BoxDecoration boxDecorationStyle = BoxDecoration(
+  color: Color.fromRGBO(230, 230, 230, 0.5),
+  borderRadius: new BorderRadius.circular(5.0),
+  );
+
+TextStyle foodNameStyle = TextStyle(color: Colors.black, fontSize: 16.0, fontWeight: FontWeight.w500);
