1efbca02257c0dd768548a15df807dbbbfc05e47
Adokiye <gorgeceo@gmail.com>
2020-03-27 14:31:55 +0100

    tweaks

---
+++ lib/components/FoodBox/RecipeFoodBox/recipeFoodBox.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'styles.dart';
+
+class RecipeFoodBox extends StatelessWidget {
+  final String text;
+  final String time;
+  final String imageUrl;
+  final GestureTapCallback onPressed;
+  AddFoodBox({@required this.text, @required this.onPressed, @required this.time, this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return new RawMaterialButton(
+      fillColor: Color.fromRGBO(230, 230, 230, 0.5),
+     // elevation: 3.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: new BorderRadius.circular(6.0),
+      ),
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.85,
+        height: 100.0,
+        padding: EdgeInsets.symmetric(horizontal: 10.0),
+        margin: EdgeInsets.only(top: 25.0),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+          CachedNetworkImage(
+   imageUrl: imageUrl,
+   placeholder: (context, url) => new CircularProgressIndicator(),
+   errorWidget: (context, url, error) => new Icon(Icons.error),
+ ),
+        ],),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
