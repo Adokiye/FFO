2c5382db6c901e3b2ad52390926bbd4910532661
Adokiye <gorgeceo@gmail.com>
2020-03-28 12:38:51 +0100

    cookingIcon.dart animation

---
+++ lib/components/Icon/CookingIcon/cookingIcon.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'styles.dart';
+
+class CookingIcon extends StatefulWidget {
+  @override
+  _CookingIconState createState() {
+    return _CookingIconState();
+  }
+}
+
+class _CookingIconState extends State<CookingIcon> {
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) => setState(() {
+          _alignment = Alignment.bottomLeft;
+        }));
+  }
+
+  AlignmentGeometry _alignment = Alignment.center;
+  _changeAlignment() {
+    setState(() {
+      _alignment = _alignment == Alignment.topRight
+          ? Alignment.bottomLeft
+          : Alignment.topRight;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      width: 66.0,
+      height: 66.0,
+      decoration: potDecorationStyle,
+      child: AnimatedAlign(
+          alignment: _alignment,
+          curve: Curves.linear,
+          duration: Duration(seconds: 2),
+          onEnd: _changeAlignment,
+          child: Container(
+            width: 45.0,
+            height: 45.0,
+            decoration: foodDecorationStyle,
+          )),
+    );
+  }
+}
---
+++ lib/components/Icon/CookingIcon/styles.dart
@@ -0,0 +1,19 @@
+
+
+import 'package:flutter/material.dart';
+
+
+BoxDecoration potDecorationStyle = BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/pot.png'),
+            fit: BoxFit.contain,
+          ),
+        );
+
+  BoxDecoration foodDecorationStyle = BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/images/food.png'),
+            fit: BoxFit.contain,
+          ),
+        );
+
