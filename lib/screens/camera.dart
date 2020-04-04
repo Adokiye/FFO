b88766ae232d005b9f9d42e5f860a50201f50c0f
Adokiye <gorgeceo@gmail.com>
2020-04-04 02:32:17 +0100

    first cc

---
+++ lib/screens/camera.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:camera/camera.dart';
+
+List<CameraDescription> cameras;
+
+class Camera extends StatefulWidget {
+  @override
+  _CameraState createState() => _CameraState();
+}
+
+class _CameraState extends State<Camera> {
+  List<CameraDescription> cameras;
+  CameraController controller;
+  bool isReady = false;
+
+  @override
+  void initState() {
+    super.initState();
+    setupCameras();
+  }
+
+  Future<void> setupCameras() async {
+    try {
+      cameras = await availableCameras();
+      controller = new CameraController(cameras[0], ResolutionPreset.medium);
+      await controller.initialize();
+    } on CameraException catch (_) {
+      setState(() {
+        isReady = false;
+      });
+    }
+    setState(() {
+      isReady = true;
+    });
+  }
+
+  Widget build(BuildContext context) {
+    if (!isReady && !controller.value.isInitialized) {
+      return Container();
+    }
+    return AspectRatio(
+        aspectRatio: controller.value.aspectRatio,
+        child: CameraPreview(controller));
+  }
+}
+}
